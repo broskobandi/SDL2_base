4218e401293cafe6f3609921a33b657447bb0477
broskobandi <broskobandi@protonmail.com>
2025-10-29 23:09:23 +0000

    Add unit tests

--- test/test.cpp
+++ test/test.cpp
@@ -15,8 +15,15 @@ int main(void) {
 			SDL_RENDERER_PRESENTVSYNC
 		);
 		CTEST(SDL_WasInit(SDL_INIT_EVERYTHING) == SDL_INIT_EVERYTHING);
-		base.load_texture("../assets/face.bmp");
-		CTEST(base.is_texture_loaded("../assets/face.bmp"));
+		auto path = "../assets/face.bmp";
+		base.load_texture(path);
+		CTEST(base.is_texture_loaded(path));
+		auto tex = base.get_texture(path);
+		base.load_texture(path);
+		std::vector<std::string_view> paths;
+		paths.push_back(path);
+		auto map = base.get_textures_map(paths);
+		CTEST(map.find(path)->second == tex);
 	} catch (const std::runtime_error& e) {
 		std::cerr << e.what() << "\n";
 	}
